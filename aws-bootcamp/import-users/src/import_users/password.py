7c96bf30c940fa974f381d5d73f8c675b5998a47
Chris King-Parra <chris@kingparra.work>
2025-06-06 08:51:50 -0400

    import-users: Rewrite generate_password

--- aws-bootcamp/import-users/src/import_users/password.py
+++ aws-bootcamp/import-users/src/import_users/password.py
@@ -2,20 +2,26 @@ import secrets
 import string
 
 
-def generate_password(length: int) -> str:
-    """Generate a password for IAM user login profile creation.
+def generate_password(
+    length: int = 30, min_lower: int = 2, min_upper: int = 2, min_digit: int = 2, min_punct: int = 2
+) -> str:
+    """Generate a password for IAM user login profile creation."""
+    if length < min_lower + min_upper + min_digit + min_punct:
+        raise ValueError("length must be greater than or equal to lower + upper + digit + punct")
 
-    The password will have at least 1 lowercase, 1 uppercase
-    and 1 digit.
-    """
-    # Punctuation characters are limited to make output csv parsing easier.
-    # No quotes, commas, backslashes, or brackets.
-    goodset = string.ascii_lowercase + string.ascii_uppercase + "!@#$%^&-_+." + string.digits
+    # Limit punctuation so output CSV is easier to parse (no commas or quotes).
+    custom_punct = "!@#$%^&-_+.[]()"
+    goodset = string.ascii_lowercase + string.ascii_uppercase + string.digits + custom_punct
+
     while True:
         password = "".join(secrets.choice(goodset) for _ in range(length))
-        has_lower = any(c.islower() for c in password)
-        has_upper = any(c.isupper() for c in password)
-        has_digit = any(c.isdigit() for c in password)
-        if has_lower and has_upper and has_digit:
+        checks = {
+            "has_min_lower": sum(c.islower() for c in password) >= min_lower,
+            "has_min_upper": sum(c.isupper() for c in password) >= min_upper,
+            "has_min_digit": sum(c.isdigit() for c in password) >= min_digit,
+            "has_min_punct": sum(c in custom_punct for c in password) >= min_punct,
+        }
+        if all(v for v in checks.values()):
             break
+
     return password
